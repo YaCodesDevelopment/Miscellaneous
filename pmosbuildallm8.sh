651aad6d9364f3bd05a9a43be5068944416245d4
YaCodesDevelopment <57478599+YaCodesDevelopment@users.noreply.github.com>
2020-08-09 14:40:42 +0300

    Add test buildscripts

---
+++ pmosbuildallm8.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+pmbootstrap shutdown
+cd ~/
+sudo rm -r /content/ya_code/local/var/pmbootstrap
+echo "!!! Set path to /content/ya_code/local/var/pmbootstrap !!!"
+echo "!!! Set path to /content/ya_code/local/var/pmbootstrap !!!"
+echo "!!! Set path to /content/ya_code/local/var/pmbootstrap !!!"
+echo "!!! Set path to /content/ya_code/local/var/pmbootstrap !!!"
+echo "!!! Set path to /content/ya_code/local/var/pmbootstrap !!!"
+pmbootstrap init
+pmbootstrap checksum device-htc-m8
+pmbootstrap checksum linux-htc-m8
+pmbootstrap checksum firmware-htc-m8
+pmbootstrap install --android-recovery-zip
+pmbootstrap export
+pmbootstrap install
+pmbootstrap export
---
+++ pmosbuildalltf201.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+pmbootstrap shutdown
+cd ~/
+sudo rm -r /content/ya_code/config_pmos
+sudo rm -r /content/ya_code/local/var/pmbootstrap
+git clone https://github.com/clamor95/postmarket_asus_tf201.git ~/config_pmos
+pmbootstrap init
+cd ~/local/var/pmbootstrap/cache_git/pmaports
+cd firmware
+cp -r ~/config_pmos/firmware-asus-tf201 ./
+cd ../device/testing/
+cp -r ~/config_pmos/device-asus-tf201 ./
+cp -r ~/config_pmos/linux-asus-tf201 ./
+cd ~/
+pmbootstrap checksum device-asus-tf201
+pmbootstrap checksum linux-asus-tf201
+pmbootstrap checksum firmware-asus-tf201
+pmbootstrap install --android-recovery-zip
+pmbootstrap export
+pmbootstrap install
+pmbootstrap export
