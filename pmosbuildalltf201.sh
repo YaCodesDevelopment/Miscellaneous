ff29b72498210ea3782f3c133c9a308ea054417e
YaCodesDevelopment <57478599+YaCodesDevelopment@users.noreply.github.com>
2020-08-11 10:50:38 +0300

    Add path patch (Clamor)

--- pmosbuildalltf201.sh
+++ pmosbuildalltf201.sh
@@ -9,7 +9,7 @@ echo "!!! Set path to /content/ya_code/local/var/pmbootstrap !!!"
 echo "!!! Set path to /content/ya_code/local/var/pmbootstrap !!!"
 echo "!!! Set path to /content/ya_code/local/var/pmbootstrap !!!"
 pmbootstrap init
-cp -r /content/ya_code/config_pmos/firmware-asus-tf201 /content/ya_code/local/var/pmbootstrap/cache_git/pmaports/firmware
+cp -r /content/ya_code/config_pmos/firmware-asus-tf201 /content/ya_code/local/var/pmbootstrap/cache_git/pmaports
 cp -r /content/ya_code/config_pmos/device-asus-tf201 /content/ya_code/local/var/pmbootstrap/cache_git/pmaports/device/testing/
 cp -r /content/ya_code/config_pmos/linux-asus-tf201 /content/ya_code/local/var/pmbootstrap/cache_git/pmaports/device/testing/
 pmbootstrap checksum device-asus-tf201
